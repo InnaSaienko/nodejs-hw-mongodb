8ebb223cce2102db33db1f13f3bde142f2f63eaf
Inna Saienko <inna.saienko.nl@gmail.com>
2025-06-04 08:24:48 +0200

    refactors the import statement which is a more concise and modern way

--- src/utils/getEnvVar.js
+++ src/utils/getEnvVar.js
@@ -1,11 +1,8 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
+import "dotenv/config";
 
 export function getEnvVar(name, defaultValue) {
     const value = process.env[name];
 
-    if(value) {return value} else if (defaultValue) {return defaultValue};
+    if(value) {return value} else if (defaultValue) {return defaultValue}
     throw new Error(`Missing: process.env['${name}']`);
 }
