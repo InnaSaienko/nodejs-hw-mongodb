00c9a5ac7abd3d7084cedcdbae1143a7de751fb3
Inna Saienko <inna.saienko.nl@gmail.com>
2025-06-04 09:00:17 +0200

    create template of contacts

---
+++ src/db/contactTemplate.js
@@ -0,0 +1,16 @@
+import { model, Schema } from 'mongoose';
+
+const contactTemplate = new Schema({
+      name: { type: String, required: true },
+      phoneNumber: { type: String, required: true },
+      email: { type: String, required: false },
+      isFavourite: { type: Boolean, default: false },
+      contactType: {
+        type: String, enum: ['work', 'home', 'personal'], required: true, default: 'personal',
+      },
+    },
+    { timestamps: true } // add keys and value createdAt and updatedAt
+    )
+;
+
+export const ContactsList = model('contacts', contactTemplate);
