561e55190d353a6cf9daa66268fb3bc92fbd972d
Inna Saienko <inna.saienko.nl@gmail.com>
2025-06-10 09:38:09 +0200

    ContactsControllers - error handling wrapper

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,28 +1,39 @@
 import { getAllContacts, getContactById } from '../services/contactService.js';
 
-export const getContactsController = async (req, res) => {
-  const contacts = await getAllContacts();
+export const getContactsController = async (req, res, next) => {
+  try {
+    const contacts = await getAllContacts();
 
-  res.json({
-    status: 200,
-    message: 'Successfully found students!',
-    data: contacts,
-  });
+    res.json({
+      status: 200,
+      message: 'Successfully found students!',
+      data: contacts,
+    });
+  } catch (e) {
+    next(e);
+  }
+
 };
 
 export const getContactsByIdController = async (req, res, next) => {
   const { contactId } = req.params;
 
-  const contact = await getContactById(contactId);
+  try {
+    const contact = await getContactById(contactId);
 
-  if (!contact) {
-   next(new Error(`Contact not found`));
-    return;
+    if (!contact) {
+      next(new Error(`Contact not found`));
+      return;
+    }
+    res.json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+
+  } catch (e) {
+    next(e);
   }
 
-  res.json({
-    status: 200,
-    message: `Successfully found contact with id ${contactId}!`,
-    data: contact,
-  });
+
 };
