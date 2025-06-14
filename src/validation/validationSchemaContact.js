945bd87dadb00e0cec6c50d0fb512d7793b30eb7
Inna Saienko <inna.saienko.nl@gmail.com>
2025-06-14 08:57:31 +0200

    validationScherma - change validation for phoneNumber

--- src/validation/validationSchemaContact.js
+++ src/validation/validationSchemaContact.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
-  phoneNumber: Joi.number().integer().min(10).max(12).required(),
+  phoneNumber: Joi.string().trim().min(10).max(12).regex(/^\+?(\d{1,3})?[-.\s]?(\d{1,4})?[-.\s]?(\d{3,4})[-.\s]?(\d{4})$/).required(),
   email: Joi.string().email().required(),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal').required(),
