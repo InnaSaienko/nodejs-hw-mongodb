56db3fcea6bc2729e42112ae0e7a2aec52d29d71
Inna Saienko <inna.saienko.nl@gmail.com>
2025-06-26 14:53:02 +0200

    refactoring arg of refresh user session fron access token to sessionID/refreshToken?

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,6 +1,5 @@
 import {loginUser, logoutUser, refreshUsersSession, registerUser} from '../services/authService.js';
 import {ONE_DAY} from "../constants/timeConstants.js";
-import {UsersCollection} from "../db/models/user.js";
 
 const setupSessionCookies = (res, session) => {
     res.cookie('refreshToken', session.refreshToken, {
@@ -48,31 +47,21 @@ export const logoutUserController = async (req, res) => {
     res.status(204).send();
 };
 
-export const refreshUserSessionController = async (req, res, next) => {
-    try {
-        const authHeader = req.headers.authorization;
-        if (!authHeader || !authHeader.startsWith("Bearer ")) {
-            return res.status(401).json({message: "No token provided"});
-        }
+export const refreshUserSessionController = async (req, res) => {
 
-        const accessToken = authHeader.split(" ")[1];
-        const session = await refreshUsersSession({accessToken});
+    const session = await refreshUsersSession({
+        sessionId: req.cookies.sessionId,
+        refreshToken: req.cookies.refreshToken,
+    });
 
-        const user = await UsersCollection.findById(session.userId).select('name email');
-        setupSessionCookies(res, session);
+    setupSessionCookies(res, session);
 
         res.json({
             status: 200,
             message: 'Successfully refreshed a session!',
             data: {
                 accessToken: session.accessToken,
-                user: {
-                    name: user.name,
-                    email: user.email
-                }
             },
         });
-    } catch (error) {
-        next(error);
-    }
+
 };
